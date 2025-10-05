2aecba5be20c860da304f36cca644775200c71ff
Juan Luis Millán Hidalgo <juanluismillanhidalgo@gmail.com>
2025-10-05 02:55:43 +0200

    reset

--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "app_name" {
-  value = fly_app.my_app.name
-}
-
-output "app_url" {
-  value = fly_app.my_app.hostname
-}
--- terraform/variables.tf
+++
@@ -1,5 +0,0 @@
-# terraform/variables.tf
-variable "fly_api_token" {
-  type        = string
-  description = "Token de Fly.io"
-}
